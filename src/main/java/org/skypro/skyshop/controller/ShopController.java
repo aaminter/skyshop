c8b5878df04315e11812a570688897b2f7427a70
aaminter <aam.interpolyaris@gmail.com>
2026-07-01 12:31:00 +0300

    Initial commit

--- src/main/java/org/skypro/skyshop/controller/ShopController.java
+++ src/main/java/org/skypro/skyshop/controller/ShopController.java
@@ -1,23 +1,34 @@
 package org.skypro.skyshop.controller;
 
 import org.skypro.skyshop.model.article.Article;
+import org.skypro.skyshop.model.basket.UserBasket;
 import org.skypro.skyshop.model.product.Product;
 import org.skypro.skyshop.model.searchresult.SearchResult;
+import org.skypro.skyshop.service.BasketService;
 import org.skypro.skyshop.service.SearchService;
 import org.skypro.skyshop.service.StorageService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
+import java.util.UUID;
 
 @RestController
 public class ShopController {
 
     private final StorageService storageService;
     private final SearchService searchService;
+    private final BasketService basketService;
 
-    public ShopController(StorageService storageService, SearchService searchService) {
+    public ShopController(StorageService storageService,
+                          SearchService searchService,
+                          BasketService basketService) {
+
         this.storageService = storageService;
         this.searchService = searchService;
+        this.basketService = basketService;
     }
 
     @GetMapping("/products")
@@ -33,5 +44,18 @@ public class ShopController {
     @GetMapping("/search")
     public Collection<SearchResult> search(@RequestParam String pattern) {
         return searchService.search(pattern);
+    }
+
+    @GetMapping("/basket/{id}")
+    public String addProduct(@PathVariable UUID id) {
+
+        basketService.addProduct(id);
+
+        return "Продукт успешно добавлен";
+    }
+
+    @GetMapping("/basket")
+    public UserBasket getUserBasket() {
+        return basketService.getUserBasket();
     }
 }
